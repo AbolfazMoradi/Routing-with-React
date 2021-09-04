c73608d8b783556f6749a21b7be75ebaa3344386
mrAbolfaz <abolfazl1.moradi7@gmail.com>
2021-09-04 16:11:13 +0430

    create functional component from App.js

--- src/maincomponent/App.js
+++ src/maincomponent/App.js
@@ -3,7 +3,7 @@ import Page1 from '../components/Page1';
 import Page2 from '../components/Page2';
 import { Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       

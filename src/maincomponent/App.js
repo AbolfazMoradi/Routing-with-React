d5212bd83edf0ae2d23bc90ba7ab24b4626b493a
mrAbolfaz <abolfazl1.moradi7@gmail.com>
2021-09-04 23:20:58 +0430

    fetch

--- src/maincomponent/App.js
+++ src/maincomponent/App.js
@@ -8,7 +8,23 @@ const App = () => {
   let [rialValue, setRialValue] = useState(0);
   let [usdValueInput, setUsdValueInput] = useState('');
 
-  
+  const fetchPrice = () => {
+    fetch('https://api.tgju.online/v1/data/sana/json')
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      let usdItem = data.sana.data.find(i => i.slug === 'sana_buy_usd');
+      let usd = parseInt(usdItem.p);
+
+      setUsdValue(usd);
+      setRialValue(parseInt(usdValueInput | 0) * usd);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   return (
     <div>App</div>
   );

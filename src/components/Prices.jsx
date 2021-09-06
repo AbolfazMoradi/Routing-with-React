dcfec6d7227614828d8add640f79e7e68c1cc760
mrAbolfaz <abolfazl1.moradi7@gmail.com>
2021-09-06 21:06:39 +0430

    fix bugs

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import CleanData from './cleanData';
 
 const Prices = (props) => {
     return(
         <div className="output-container">
             <div className="rial-price-output">Price: <strong>{props.rialValue}</strong> <small> rial</small></div>
             <div className="usd-price-output">USD: <strong>{props.usdValue}</strong> <small> USD</small></div>
-
-            
         </div>
     );
 }
